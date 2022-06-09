ff1c3371473e2b303d911e3b52d656669420dded
Sara Ibarra <saraiba33@gmail.com>
2022-06-09 09:14:54 -0600

    Connect remote db

--- propertylist.sql
+++ propertylist.sql
@@ -1,1 +1,5 @@
-INSERT INTO property_
+TRUNCATE propertydata;
+
+INSERT INTO propertydata (id, image, address, status, end_date) VALUES(
+    1, 'https://ap.rdcpix.com/7e1f558e2f6a5e5f5611add7cc966e76l-m2099888312s-w642_h420_q75.webp', '7365 Forest Meadows Ave', 'Rented', '06/30/2022'),(
+    2, 'https://ap.rdcpix.com/7e1f558e2f6a5e5f5611add7cc966e76l-m2099888312s-w642_h420_q75.webp', '7365 Forest Meadows Ave', 'Rented', '06/30/2022');
